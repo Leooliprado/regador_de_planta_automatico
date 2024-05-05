f2686de5d0a2892f2d3f788e65fe49dfa79b6012
Leooliprado <leonardooliveiraprado2005@gmail.com>
2024-05-05 16:36:01 -0300

    leo

---
+++ regador_de_planta_automatico.ino
@@ -0,0 +1,58 @@
+/* Regador automático para planta. 20º projeto.linguagem utilizada c++
+ <<Projeto de robótica do ditão 2023>>
+ 1º campeonato "Ditão robótica"
+
+ criado por: Leonardo de Oliveira Prado.
+ Instagram: arduino2.0tecnologico
+
+ Data de inicio do projeto 03/09/2023
+ Data de término do projeto 05/05/2024  
+
+************************************************************************************************************************************
+*/
+#define vermelho 2
+#define amarelo 3
+#define verde 4
+#define bombadagua 5
+#define SensorDeUmidade A0
+
+void setup() {
+  pinMode(SensorDeUmidade, INPUT);  // sensor de umidade da terra
+
+  pinMode(vermelho, OUTPUT);  //pin vermelho 2
+  pinMode(amarelo, OUTPUT);   //pin vermelho 3
+  pinMode(verde, OUTPUT);     //pin vermelho 4
+
+  pinMode(bombadagua, OUTPUT);  //pin boba d'água 5
+  //Serial.begin(9600);
+}
+
+void loop() {
+  //Serial.println(analogRead(A1));
+  //delay(50);
+  if (analogRead(SensorDeUmidade) <= 1023 && analogRead(SensorDeUmidade) >= 601) {  // ligar a bomba d'água //umidade da terra ruim
+    digitalWrite(amarelo, LOW);
+    digitalWrite(verde, LOW);
+    digitalWrite(vermelho, HIGH);
+
+    digitalWrite(bombadagua, HIGH);
+    delay(1500);
+  }
+
+  else if (analogRead(SensorDeUmidade) <= 600 && analogRead(SensorDeUmidade) >= 501) {  // desligar a bomba d'água //umidade da terra média
+    digitalWrite(vermelho, LOW);
+    digitalWrite(verde, LOW);
+    digitalWrite(amarelo, HIGH);
+
+    digitalWrite(bombadagua, LOW);
+    delay(1500);
+
+  } else if (analogRead(SensorDeUmidade) <= 500) {  // desligar a bomba d'água //umidade da terra boa
+    digitalWrite(amarelo, LOW);
+    digitalWrite(vermelho, LOW);
+    digitalWrite(verde, HIGH);
+
+    digitalWrite(bombadagua, LOW);
+    delay(1500);
+  }
+}
