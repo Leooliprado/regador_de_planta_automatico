0ca8d3abca1163690eef27b366321975505f3616
Leooliprado <leonardooliveiraprado2005@gmail.com>
2024-05-18 12:59:18 -0300

    leo1805241259

--- regador_de_planta_automatico.ino
+++ regador_de_planta_automatico.ino
@@ -13,7 +13,9 @@
 #define vermelho 2
 #define amarelo 3
 #define verde 4
-#define bombadagua 5
+#define bombadagua1 5
+#define bombadagua2 6
+#define bombadagua3 9
 #define SensorDeUmidade A0
 
 void setup() {
@@ -23,39 +25,45 @@ void setup() {
   pinMode(amarelo, OUTPUT);   //pin vermelho 3
   pinMode(verde, OUTPUT);     //pin vermelho 4
 
-  pinMode(bombadagua, OUTPUT);  //pin boba d'água 5
+  pinMode(bombadagua1, OUTPUT);  //pin boba d'água 5
+  pinMode(bombadagua2, OUTPUT);  //pin boba d'água 6
+  pinMode(bombadagua3, OUTPUT);  //pin boba d'água 9
+  analogWrite(bombadagua3,60);
   //Serial.begin(9600);
 }
 
 void loop() {
-  //Serial.println(analogRead(A1));
+  //Serial.println(analogRead(SensorDeUmidade));
   //delay(50);
   if (analogRead(SensorDeUmidade) <= 1023 && analogRead(SensorDeUmidade) >= 601) {  // ligar a bomba d'água //umidade da terra ruim
-    delay(2000);
+   
     digitalWrite(amarelo, LOW);
     digitalWrite(verde, LOW);
     digitalWrite(vermelho, HIGH);
-
-    digitalWrite(bombadagua, HIGH);
-    delay(2000);
+    
+    digitalWrite(bombadagua1, HIGH);
+    digitalWrite(bombadagua2, LOW);
+   
   }
 
   else if (analogRead(SensorDeUmidade) <= 600 && analogRead(SensorDeUmidade) >= 501) {  // desligar a bomba d'água //umidade da terra média
-    delay(2000);
+  
     digitalWrite(vermelho, LOW);
     digitalWrite(verde, LOW);
     digitalWrite(amarelo, HIGH);
 
-    digitalWrite(bombadagua, LOW);
-    delay(2000);
+    digitalWrite(bombadagua1, LOW);
+    digitalWrite(bombadagua2, LOW); 
+   
 
   } else if (analogRead(SensorDeUmidade) <= 500) {  // desligar a bomba d'água //umidade da terra boa
-    delay(2000);
+  
     digitalWrite(amarelo, LOW);
     digitalWrite(vermelho, LOW);
     digitalWrite(verde, HIGH);
 
-    digitalWrite(bombadagua, LOW);
-    delay(2000);
+    digitalWrite(bombadagua1, LOW);
+    digitalWrite(bombadagua2, LOW); 
   }
+  delay(2000);
 }
